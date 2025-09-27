90eb96b32fc60976667ef7495f1137d6cbf3ee87
Vinicius Saraiva <vinicius@s4r41va.com>
2025-09-27 10:19:09 -0300

    env

--- src/main/java/br/com/fatecwebsystem/websystem/WebsystemApplication.java
+++ src/main/java/br/com/fatecwebsystem/websystem/WebsystemApplication.java
@@ -1,5 +1,6 @@
 package br.com.fatecwebsystem.websystem;
 
+import io.github.cdimascio.dotenv.Dotenv; // <-- ADICIONE ESTE IMPORT
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebsystemApplication {
 
 	public static void main(String[] args) {
+
+		// Carrega as variáveis do arquivo .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Configura as variáveis de ambiente para a aplicação Spring
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+
 		SpringApplication.run(WebsystemApplication.class, args);
 	}
 
